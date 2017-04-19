b63892cc26d8e40698ea830774f0a2e88b97ad7e
Merik Westerveld <337441@student.fontys.nl>
2017-04-19 12:44:32 +0200

    Vragen 3 & 4.

--- Assigment 1/bank/src/test/java/util/Account_Test.java
+++ Assigment 1/bank/src/test/java/util/Account_Test.java
@@ -96,5 +96,72 @@ public class Account_Test {
         
         assertEquals(0, accountDataAccess.count());
     }
+    
+    /**
+     * 1. Wat is de waarde van asserties en printstatements?
+     * expected == account.getId() == -100L == -100L == true. 
+     * expected == account.getId() == -100L == 16L == true (door de NotEquals).
+     * 
+     * Doordat er pas later geflusht wordt, wordt er een autoincrement op de database gedaan waardoor het ID veranderd wordt. 
+     * Hierdoor is deze later anders.
+     * 
+     * 2. Welke SQL statements worden gegenereerd? 
+     * Na de .flush() wordt INSERT INTO ACCOUNT (ACCOUNTNR, BALANCE, THRESHOLD) VALUES (?, ?, ?) bind => [111, 0, 0] uitgevoerd.
+     * Na de .getId() wordt SELECT LAST_INSERT_ID() uitgevoerd.
+     * 
+     * 3. Wat is het eindresultaat in de database? 
+     * Er wordt een nieuw account in de database gezet met een ID van 16 en niet -100.
+     */
+    @Test
+    public void test_vraag3(){
+        Long expected = -100L;
+        Account account = new Account(111L);
+        account.setId(expected);
+        em.getTransaction().begin();
+        em.persist(account);
+
+        assertEquals(expected, account.getId());
+        em.flush();
+
+        assertNotEquals(expected, account.getId());
+        em.getTransaction().commit();
+    }
+    
+    /**
+     * 1. Wat is de waarde van asserties en printstatements?
+     * ExpectedBalance == account.getBalance() == 400L == 400L == true.
+     * De entity Account wordt gepersisteerd en daarna nog veranderd. Pas na de commit wordt het ook
+     * daadwerkelijk in de database gezet. 
+     * 
+     * expectedBalance == found.getBalance() == 400L == 400L == true.
+     * Het gevonden account heeft in de database een balance van 400 en is daarom true.
+     * 
+     * 2. Welke SQL statements worden gegenereerd? 
+     * Na de commit() wordt INSERT INTO ACCOUNT (ACCOUNTNR, BALANCE, THRESHOLD) VALUES (?, ?, ?) bind => [114, 400, 0] uitgevoerd.
+     * Bij find() wordt SELECT ID, ACCOUNTNR, BALANCE, THRESHOLD FROM ACCOUNT WHERE (ID = ?) bind => [29] uitgevoerd.
+     * 
+     * 3. Wat is het eindresultaat in de database? 
+     * In de database zit een nieuw record van een klant met ID 29 en balans 400 en accountnr 114. 
+     */
+    @Test
+    public void test_vraag4(){
+        Long expectedBalance = 400L;
+        Account account = new Account(114L);
+        em.getTransaction().begin();
+        em.persist(account);
+        account.setBalance(expectedBalance);
+        System.out.println("2");
+        em.getTransaction().commit();
+        System.out.println("3");
+        assertEquals(expectedBalance, account.getBalance());
+
+        Long cid = account.getId();
+        account = null;
+        EntityManager em2 = factory.createEntityManager();
+        em2.getTransaction().begin();
+        Account found = em2.find(Account.class,  cid);
+
+        assertEquals(expectedBalance, found.getBalance());
+    }
 
 }
