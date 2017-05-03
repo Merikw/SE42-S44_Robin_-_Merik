a5d65b58483fdd0e58c7f4113914695d990fa576
Merik Westerveld <337441@student.fontys.nl>
2017-05-03 11:36:50 +0200

    Starting assigment 3
    Bijgevoegd zit het mapje met de kopie van assigment 2 om te beginnen aan assigment 3.

---
+++ Assigment 3/auction/src/main/java/auction/domain/Item.java
@@ -0,0 +1,68 @@
+package auction.domain;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import nl.fontys.util.Money;
+
+public class Item implements Comparable, Serializable {
+
+    private Long id;
+    private User seller;
+    private Category category;
+    private String description;
+    private Bid highest;
+
+    public Item() {
+        // Empty constructor used for JPA binding.
+    }
+
+    public Item(User seller, Category category, String description) {
+        this.seller = seller;
+        this.category = category;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getSeller() {
+        return seller;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Bid getHighestBid() {
+        return highest;
+    }
+
+    public Bid newBid(User buyer, Money amount) {
+        if (highest != null && highest.getAmount().compareTo(amount) >= 0) {
+            return null;
+        }
+        highest = new Bid(buyer, amount);
+        return highest;
+    }
+
+    public int compareTo(Object arg0) {
+        //TODO
+        return -1;
+    }
+
+    public boolean equals(Object o) {
+        //TODO
+        return false;
+    }
+
+    public int hashCode() {
+        //TODO
+        return 0;
+    }
+}
